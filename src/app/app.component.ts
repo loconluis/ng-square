f5e751c54ecd1fda6dddc467de0abf96117a69ff
luislocon <luis.locon@gmail.com>
2017-09-11 23:14:54 -0600

    Adding google maps module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,21 @@ export class AppComponent {
     {active: false, nombre: 'Sushi Sushiroll'},
     {active: true, nombre: 'Hotel la Gracia'},
     {active: false, nombre: 'Zapateria el Clavo'},
+  ];
+
+  personas:any = [
+    {nombre: 'Luis Locon', edad: 24 },
+    {nombre: 'Monica Locon', edad: 28 },
+    {nombre: 'Away León', edad: 32 },
+    {nombre: 'Ileana M', edad: 16 },
+    {nombre: 'Gato Ramirez', edad: 17 },
+    {nombre: 'Carlos Herrera', edad: 22 },
+    {nombre: 'Kike Pavón', edad: 31 }
   ]
+
+  lat:number = 14.5082397;
+  lng:number = -90.5733147;
+
 
   constructor() {
     
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDTgyF31xIwlLbRSktZSxBA4eg9Lc6IuHY'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
