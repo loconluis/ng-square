cd94d680ba596e6e19df5b8fba45f0292b919703
luislocon <luis.locon@gmail.com>
2017-09-11 23:41:19 -0600

    Using ngSwitch directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,23 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Platzisquare';
   lugares:any = [
-    {active: true, nombre: 'Floreria la gardenia'},
-    {active: true, nombre: 'Donas la pasadita'},
-    {active: true, nombre: 'Veterinaria Huellitas Felices'},
-    {active: false, nombre: 'Sushi Sushiroll'},
-    {active: true, nombre: 'Hotel la Gracia'},
-    {active: false, nombre: 'Zapateria el Clavo'},
+    {cercania: 1, distancia: 1, active: true, nombre: 'Floreria la gardenia'},
+    {cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita'},
+    {cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria Huellitas Felices'},
+    {cercania: 3, distancia: 10, active: false, nombre: 'Sushi Sushiroll'},
+    {cercania: 3, distancia: 35, active: true, nombre: 'Hotel la Gracia'},
+    {cercania: 3, distancia: 120, active: false, nombre: 'Zapateria el Clavo'},
   ];
-
-  personas:any = [
-    {nombre: 'Luis Locon', edad: 24 },
-    {nombre: 'Monica Locon', edad: 28 },
-    {nombre: 'Away León', edad: 32 },
-    {nombre: 'Ileana M', edad: 16 },
-    {nombre: 'Gato Ramirez', edad: 17 },
-    {nombre: 'Carlos Herrera', edad: 22 },
-    {nombre: 'Kike Pavón', edad: 31 }
-  ]
 
   lat:number = 14.5082397;
   lng:number = -90.5733147;
