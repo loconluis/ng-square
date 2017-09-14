9b0ef6d8558908a972d0e6391209546282423a55
luislocon <luis.locon@gmail.com>
2017-09-13 23:41:25 -0600

    Mandando parametros desde routeLink

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 export class LugaresComponent {
   title = 'Platzisquare';
   lugares:any = [
-    {plan: 'pagado', cercania: 1, distancia: 1, active: true, nombre: 'Floreria la gardenia'},
-    {plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita'},
-    {plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria Huellitas Felices'},
-    {plan: 'gratuito', cercania: 3, distancia: 10, active: false, nombre: 'Sushi Sushiroll'},
-    {plan: 'pagado', cercania: 3, distancia: 35, active: true, nombre: 'Hotel la Gracia'},
-    {plan: 'gratuito', cercania: 3, distancia: 120, active: false, nombre: 'Zapateria el Clavo'},
+    {id: 1 , plan: 'pagado', cercania: 1, distancia: 1, active: true, nombre: 'Floreria la gardenia'},
+    {id: 2 , plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita'},
+    {id: 3 , plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria Huellitas Felices'},
+    {id: 4 , plan: 'gratuito', cercania: 3, distancia: 10, active: false, nombre: 'Sushi Sushiroll'},
+    {id: 5 , plan: 'pagado', cercania: 3, distancia: 35, active: true, nombre: 'Hotel la Gracia'},
+    {id: 6 , plan: 'gratuito', cercania: 3, distancia: 120, active: false, nombre: 'Zapateria el Clavo'},
   ];
 
   lat:number = 14.5082397;
