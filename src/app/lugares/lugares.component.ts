033249dc8202b40339cb0449daa8d58b82a0056c
luislocon <luis.locon@gmail.com>
2017-09-15 01:38:48 -0600

    Añadiendo animacion a mi app

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -9,31 +9,24 @@ import { LugaresService } from '../services/lugares.service';
   animations: [
     trigger('contenedorAnimable', [
       state('inicial', style({
-        opacity: 0, 
-        backgroundColor: 'green',
-        transform: 'rotate3d(0, 0, 0, 0deg)'
+        opacity: 0
       })),
       state('final', style({
-        opacity: 1, 
-        backgroundColor: 'yellow',
-        transform: 'rotate3d(5,10,20,30deg)'
+        opacity: 1
       })),
-      transition('inicial => final', animate(1000)),
+      transition('inicial => final', animate(3000)),
       transition('final => inicial', animate(500)),
     ])
   ]
 })
 export class LugaresComponent {
   title = 'Platzisquare';
-  state = 'final';
+  state = 'inicial';
 
   lat:number = 14.5082397;
   lng:number = -90.5733147;
   lugares = null;
 
-  animar() {
-    this.state = (this.state === 'final') ? 'inicial' : 'final';
-  }
 
   constructor(private lugaresService: LugaresService) {
     lugaresService.getLugares()
@@ -42,6 +35,7 @@ export class LugaresComponent {
         let me = this;
         // pasar de objeto a un arreglo
         me.lugares = Object.keys(me.lugares).map(function (key) { return me.lugares[key] });
+        this.state = 'final';
       }, error => { console.log(error);
       alert('Tenemos algo de dificultades , disculpe las molestias. Error: '+error.statusText) });
   }
